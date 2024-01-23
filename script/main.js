8f5da62831a0a5cdfd640f7ce27c091d4155602e
davidemarchini <marchini73@gmail.com>
2024-01-23 16:37:39 +0100

    stampo valori chiavi

--- script/main.js
+++ script/main.js
@@ -1,1 +1,54 @@
+// #### NUOVA VARIABILE DI ARRAY DI OGGETTI
+
+
+
+const team = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+  
+  // #### STAMPO SU CONSOLE I VALORI DELLE CHIAVI
+  
+  console.log(team[0].name);
+
+  for (const oggetto of team) {
+    console.log(oggetto)
+    
+  }
+
+//   ### stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
+
+
+
 
